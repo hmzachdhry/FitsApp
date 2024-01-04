5a6093b1c3a7982c9d12397b3a3af6d07f52f17b
hmz44 <126123010+hmz44@users.noreply.github.com>
2024-01-04 13:52:46 -0500

    welcome screen added

--- src/AppNavigator.jsx
+++ src/AppNavigator.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import SignUpScreen from './screens/SignUpScreen'
 
 const Stack = createStackNavigator();
 
