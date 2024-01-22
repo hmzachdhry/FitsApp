29e5d122dd14bef5d8260173604bf6d90568bfd2
hmz44 <126123010+hmz44@users.noreply.github.com>
2024-01-22 11:31:32 -0500

    homescreen added

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,33 @@
-import { useNavigation } from '@react-navigation/native';
-import React, {useState} from 'react';
-import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
+const handleLogin = async () => {
+  const navigation = useNavigation(); 
 
+  try {
+    // Make a request to backend for auth
+    const response = await fetch('insert backend URL here', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username, password}),
+    });
 
-const navigation = useNavigation()
+    if (response.ok) {
+      // If authentication is successful
+      const data = await response.json();
+      Alert.alert('Login Successful', `Welcome back, ${data.username}!`);
 
+      // Navigate to HomeScreen 
+      navigation.navigate('Home'); 
+    } else {
+      // If auth fails
+      Alert.alert('Login Failed', 'Invalid username or password.');
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle error 
+  }
+};
 
-
-
-
-
+export default HomeScreen;
